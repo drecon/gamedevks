041b3add89724992ae1c08ef5c44917c569f0217
drecon <drecon@drecon.com.br>
2014-02-05 12:55:32 -0200

    images

---
+++ run.bat
@@ -0,0 +1,20 @@
+rem Install ImageMagick and Ghostscript to work!
+rem insert global path
+rem rename to gs.exe 
+convert -density 600 ./pdf/balance.pdf ./img/balance.png
+convert -density 600 ./pdf/cast.pdf ./img/cast.png
+convert -density 600 ./pdf/character.pdf ./img/character.png
+convert -density 600 ./pdf/cutscene.pdf ./img/cutscene.png
+convert -density 600 ./pdf/gameplay.pdf ./img/gameplay.png
+convert -density 600 ./pdf/heros_journey.pdf ./img/heros_journey.png
+convert -density 600 ./pdf/history.pdf ./img/history.png
+convert -density 600 ./pdf/interface.pdf ./img/interface.png
+convert -density 600 ./pdf/items.pdf ./img/items.png
+convert -density 600 ./pdf/iterations.pdf ./img/iterations.png
+convert -density 600 ./pdf/resume.pdf ./img/resume.png
+convert -density 600 ./pdf/rules.pdf ./img/rules.png
+convert -density 600 ./pdf/scene.pdf ./img/scene.png
+convert -density 600 ./pdf/sketch.pdf ./img/sketch.png
+convert -density 600 ./pdf/talk.pdf ./img/talk.png
+convert -density 600 ./pdf/technologies.pdf ./img/technologies.png
+pause
